61d4356cf3cf46668b68b43549cc17886f6e2bc9
Tim <timmn@mail.ru>
2025-03-24 12:07:32 +0300

    Асинхронная задержка убрал

--- app/services/mail/temp_mail_tm.py
+++ app/services/mail/temp_mail_tm.py
@@ -2,7 +2,6 @@ import asyncio
 from mailtm import Email
 
 async def create_mail():
-    await asyncio.sleep(3000)  # Асинхронная задержка
     test = Email()
     test.register()
     return test.address, test.token
