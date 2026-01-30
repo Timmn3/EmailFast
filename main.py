c3efb355fc0359c7077c341f9d8fed20e86b5c05
Tim <timmn@mail.ru>
2026-01-31 00:37:03 +0300

    Версия 31.01.2026

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ import signal
 import logging
 
 # Версия для отображения/отладки
-msg_text = "Версия 28.01.2026"
+msg_text = "Версия 31.01.2026"
 
 from contextlib import suppress
 from aiogram.types import Message, CallbackQuery
