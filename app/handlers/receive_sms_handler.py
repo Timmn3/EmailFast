00cf4aca5d977a63d459eb4b53e66752d392c66e
Tim <timmn@mail.ru>
2026-01-30 14:42:04 +0300

    await bot.send_message

--- app/handlers/receive_sms_handler.py
+++ app/handlers/receive_sms_handler.py
@@ -412,7 +412,7 @@ async def cancel_service(call: types.CallbackQuery, **kwargs):
 
         # ✅ Сразу отвечаем на callback (чтобы не висел "часик")
         # await call.answer(text=answer_text or "Отмена больше не доступна")
-        await bot.send_message(chat_id=user_id, text=answer_text or "Отмена больше не доступна")
+        await bot.send_message(chat_id=user_id, text=answer_text)
 
         # 🧹 UX: убираем клавиатуру у конкретного сообщения, по которому нажали
         if need_clear_kb:
