5519da1e52d3f14cc0b3cf56680ff5ec48cb93cc
Tim <timmn@mail.ru>
2026-01-31 10:56:56 +0300

    tiktok

--- app/dependencies.py
+++ app/dependencies.py
@@ -150,7 +150,7 @@ SMSFAST_SERVICE_MAP = {
     "telegram": "tg",
     "vkcom":    "vk",
     "google":   "go",
-    "tiktok":   "if",
+    "tiktok":   "lf",
     "amazon":   "am",
     "claude":   "acz",
     "ot":       "ot",   # "Любой другой"
